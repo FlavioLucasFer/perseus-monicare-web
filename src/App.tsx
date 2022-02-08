29afaf18fa6ab3d2fb136e8becbe1cac9cbeb36e
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-02-08 16:37:37 -0300

    Implement core navigation #13

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+	Routes,
+	Route,
+} from "react-router-dom";
+
 import Login from 'screens/Login';
+import Measurements from 'screens/Measurements';
 
 function App() {
   return (
-    <Login />
+		<Routes>
+			<Route path='/'>
+				<Route index element={<Login />}/>
+				
+				<Route path='patients'>
+					<Route path='measurements' element={<Measurements />} />
+				</Route>
+			</Route>
+		</Routes>
   );
 }
 
