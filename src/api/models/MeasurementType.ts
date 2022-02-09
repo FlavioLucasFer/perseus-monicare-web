950fff3be10ed78b263c7bb6c4ab7dd729793989
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-02-09 18:58:42 -0300

    MeasurementType and PatientMeasurement models

---
+++ src/api/models/MeasurementType.ts
@@ -0,0 +1,18 @@
+export type PatientMeasurementStatusT = 'bad' | 'caution' | 'good';
+
+export type MeasurementTypeAttributesT = {
+	id: number,
+	name: string,
+};
+
+class MeasurementType {
+	public readonly id: number;
+	public readonly name: string;
+
+	public constructor(attributes: MeasurementTypeAttributesT) {
+		this.id = attributes.id;
+		this.name = attributes.name;
+	}
+};
+
+export default MeasurementType;
