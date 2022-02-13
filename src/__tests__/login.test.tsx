6cf608b4c5e9724bf3ced96c82f8e0668198d551
angelMizutani <angelisayuri@yahoo.com.br>
2022-02-13 18:14:15 -0300

    implemented snapshot test for login screen

--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { render, cleanup, fireEvent, screen } from '@testing-library/react';
 import { shallow } from 'enzyme';
-import { Routes } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import Login from 'screens/Login';
 
-afterEach(cleanup);
 const mockedUseNavigate = jest.fn();
 
 jest.mock('react-router-dom', () => ({
-	...jest.requireActual('react-router-dom'), 
+	...jest.requireActual('react-router-dom') as any, 
 	useNavigate: () => mockedUseNavigate,
 }));
 
 describe('Login Screen', () => {
 	it('Should render properly', () => {
-		const wrapper = shallow(<Login/>);
+		const wrapper = shallow(
+			<Login/>
+		);
 		expect(wrapper.debug()).toMatchSnapshot();
 
 	}); 
 
-	// const linkElement = screen.getByText('Esqueci minha senha');
-	// expect(linkElement).toBeInTheDocument();
-
-    
-	// const {debug, getByText} = render(<Login/>)
-	// expect(getByText('Entrar').tagName).toBe('BUTTON')
-	// debug()
 });
